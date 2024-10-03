1c8f14127706b8d5c52981f5911a5d6a141642fa
Tran Minh Quan <steammicharel@gmail.com>
2024-10-03 10:43:34 +0700

    add FE login

---
+++ RBN_FE/RBN_FE/Pages/LogIn&Out/Logout.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RBN_FE.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnPost()
+        {
+            // Clear all session data
+            HttpContext.Session.Clear();
+            // Explicitly delete the session cookie
+            Response.Cookies.Delete(".AspNetCore.Session");
+            // Redirect to the home page or login page
+            return RedirectToPage("/Index");
+        }
+    }
+}
--- RBN_FE/RBN_FE/Program.cs
+++ RBN_FE/RBN_FE/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+// Add session support
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins",
@@ -13,18 +27,38 @@ builder.Services.AddCors(options =>
                   .AllowAnyMethod();
         });
 });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // Uncomment the following line to enable HTTPS redirection in production
+    // app.UseHsts();
 }
+
 app.UseCors("AllowAllOrigins");
 
-app.UseStaticFiles();
+// Uncomment the following line to enable HTTPS redirection
+// app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseRouting();
+
+// Add session middleware
+app.UseSession();
+
+app.Use(async (context, next) =>
+{
+    var token = context.Session.GetString("JWTToken");
+    if (!string.IsNullOrEmpty(token))
+    {
+        context.Request.Headers.Add("Authorization", "Bearer " + token);
+    }
+    await next();
+});
 
 app.UseAuthorization();
 
